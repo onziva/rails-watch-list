effb1242f0525042ce4da7998d13c2c5773e065a
David Rousseau <onziva@proton.me>
2026-05-21 17:15:06 +0200

    exo parties 1 à 4

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,30 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require "json"
+require "open-uri"
+
+puts "Nettoyage de la base..."
+Bookmark.destroy_all
+List.destroy_all
+Movie.destroy_all
+
+puts "Chargement des films..."
+url = "https://tmdb.lewagon.com/movie/top_rated"
+movies_data = JSON.parse(URI.open(url).read)["results"]
+
+movies_data.each do |movie|
+  next if movie["overview"].blank?
+
+  Movie.find_or_create_by!(title: movie["title"]) do |m|
+    m.overview   = movie["overview"]
+    m.poster_url = "https://image.tmdb.org/t/p/w500#{movie["poster_path"]}"
+    m.rating     = movie["vote_average"]
+  end
+end
+
+puts "#{Movie.count} films chargés."
+
+puts "Création des listes..."
+[ "À voir ce soir", "Films cultes", "En famille" ].each do |name|
+  List.find_or_create_by!(name: name)
+end
+
+puts "#{List.count} listes créées."
